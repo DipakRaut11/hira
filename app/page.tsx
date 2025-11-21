fa1fa8c47308a3a5a8403542c5639c7e9f9ac402
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-11-21 07:10:08 +0545

    feat(home): add contact us page link

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <HeroImageCarousel />
          <AboutSection /> 
          <ServiceList />
-         {/* <ContactInfo /> */}
+         <ContactInfo />
 
     </main>
   );
